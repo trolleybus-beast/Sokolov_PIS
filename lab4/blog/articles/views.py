541676979cbcba9b860f612a41fb84ba63ec4041
trolleybus-beast <48729647+trolleybus-beast@users.noreply.github.com>
2026-03-16 15:41:48 +0300

    lab4

---
+++ lab4/blog/articles/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from .models import Article
+from django.http import Http404
+
+def archive(request):
+    posts = Article.objects.all()
+    return render(request, 'archive.html', {"posts": posts})
+
+
+def get_article(request, article_id):
+    try:
+        post = Article.objects.get(id=article_id)
+        return render(request, 'article.html', {"post": post})
+    except Article.DoesNotExist:
+        raise Http404
